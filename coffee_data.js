46c3334f5cb9b603befae7f808d66322e6b51e5e
Patsy O'Brien <97324605+patsylin@users.noreply.github.com>
2023-07-19 19:23:54 -0500

    updated

--- coffee_data.js
+++ coffee_data.js
@@ -8,51 +8,64 @@
 // d. Connect the coffee_data.js file to the index.js file.
 
 const coffeeMenu = [
-    {
-        name: "cappuccino",
-        price: "8",
-        seasonal: false,
-    },
-    {
-        name: espresso,
-        price: 5,
-        seasonal: false,
-    }
-    {
-        name: "latte",
-        price: 5,
-        seasonal: false,
-    },
-    {
-        name: affogato,
-        price: "9",
-        seasonal: true,
-    },
-    {
-        name: "macchiato",
-        price: "6",
-        seasonal: false,
-    }
-    {
-        name: americano,
-        price: 7,
-        seasonal: false,
-    }
-    {
-        name: "iced coffee",
-        price: "6",
-        seasonal: false,
-    },
-    {
-        name: frappe,
-        price: "8",
-        seasonal: false,
-    }
-    {
-        name: "cuban espresso",
-        price: 10,
-        seasonal: true,
-    }
-]
+  {
+    name: "cappuccino",
+    price: "8",
+    seasonal: false,
+  },
+  {
+    name: "espresso",
+    price: 5,
+    seasonal: false,
+  },
+  {
+    name: "latte",
+    price: 5,
+    seasonal: false,
+  },
+  {
+    name: "affogato",
+    price: "9",
+    seasonal: true,
+  },
+  {
+    name: "macchiato",
+    price: "6",
+    seasonal: false,
+  },
+  {
+    name: "americano",
+    price: 7,
+    seasonal: false,
+  },
+  {
+    name: "iced coffee",
+    price: "6",
+    seasonal: false,
+  },
+  {
+    name: "frappe",
+    price: "8",
+    seasonal: false,
+  },
+  {
+    name: "cuban espresso",
+    price: 10,
+    seasonal: true,
+  },
+];
 
-module.exports = coffeeMenu
+module.exports = coffeeMenu;
+
+//Create a function that loops through the obj
+//making these changes
+//all prices should be numbers
+//all items shout be strings
+
+function cleanCoffeeMenu() {
+  for (const coffeeItem of coffeeMenu) {
+    coffeeItem.price = Number(coffeeItem.price); // Convert price to a number
+  }
+}
+cleanCoffeeMenu();
+console.log(coffeeMenu);
